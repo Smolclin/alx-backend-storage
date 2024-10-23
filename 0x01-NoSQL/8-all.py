976a6873fff85b916e01895b8697f3f2e4e89253
Smolclin <clinsmol10@gmail.com>
2024-10-23 15:10:36 -0700

    List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,6 +6,7 @@ Python function that lists documents
 
 def list_all(mongo_collection):
     """ lists python documents """
+    documents = mongo_collection.find()
 
     if documents.count() == 0:
         return[]
