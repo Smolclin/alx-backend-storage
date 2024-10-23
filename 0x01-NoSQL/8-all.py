b2bb580c6bac41b164e685bb36d72cd7fdffe837
Smolclin <clinsmol10@gmail.com>
2024-10-23 15:06:02 -0700

    List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" 
+Python function that lists documents
+"""
+
+def list_all(mongo_collection):
+    """ lists python documents """
+
+    if documents.count() == 0:
+        return[]
+
+     return documents
