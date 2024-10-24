02bbf78225e5dcadd94c4953093395682ef2cab2
Smolclin <clinsmol10@gmail.com>
2024-10-23 21:51:22 -0700

    Insert a document in Python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,5 +4,7 @@
 Python function that inserts a new document in a collection
 """
 
+
 def insert_school(mongo_collection, **kwargs):
+    """ inserting """
     return mongo_collection.insert(kwargs)
