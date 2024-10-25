44c0317b0139547bc5ed34332731f937e1bd7f80
Smolclin <clinsmol10@gmail.com>
2024-10-25 11:08:58 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,14 +10,16 @@ from typing import Any, Callable, Optional, Union
 
 
 class Cache:
-    def __init__(self, host'=localhost', port=6379, db=0):
+    def __init__(self, host='localhost', port=6379, db=0):
         """
+        catching data
         """
         self._redis = redis.Redis(host=host, port=port, db=db)
         self._redis.flushdb()
 
     def store(self, data: union[str, bytes, int, float]) -> str:
         """
+        storing data
         """
         r_key = str(uuid.uuid4())
         self.__redis.set(r_key, data)
